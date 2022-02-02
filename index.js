2cf36f5eb5c7eb903541815bbe3401fee9b4e0f1
mkervabo <mkervabo@student.42.fr>
2022-02-02 12:04:43 +0100

    update weather every 30 seconde

--- index.js
+++ index.js
@@ -12,34 +12,41 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-	socket.on('chat message', (msg) => {
-		country = wcc.getCountryDetailsByName(wcc.getRandomCountry());
-		
-		const options = {
-			method: 'GET',
-			url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-			qs: {
-				q: `${country[0].capital}`,
-				units: 'metric',
-			},
-			headers: {
-				'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-				'x-rapidapi-key': '[your rapidapi key]',
-				useQueryString: true
-			}
-		};
+function getWeather () {
+	country = wcc.getCountryDetailsByName(wcc.getRandomCountry());
 
-		request(options, function (error, response, body) {
-			if (error) throw new Error(error);
+	const options = {
+		method: 'GET',
+		url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+		qs: {
+			q: `${country[0].capital}`,
+			units: 'metric',
+		},
+		headers: {
+			'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+			'x-rapidapi-key': '[your rapidapi key]',
+			useQueryString: true
+		}
+	};
 
-			const weatherInfo = JSON.parse(body);
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
 
-			io.emit('chat message', body);
-		});
-		// io.emit('chat message', msg);
+		const weatherInfo = JSON.parse(body);
+		io.emit('weather info', body);
 	});
+	
+};
+
+io.on('connection', (socket) => {
+	getWeather();
+	setInterval(() => {
+		getWeather()},
+		30000);
+	
 });
+
+// setInterval(io.emit('weather info', "coucou"), 10000);
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
